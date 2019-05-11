a08df038a1ca88e01e093a8e038ee639a9e18199
argenis0409 <argenis04_09@icloud.com>
2019-05-11 11:01:20 -0700

    express server

---
+++ server/README.md
@@ -0,0 +1,5 @@
+Express Server.
+
+in this project i learned how to save data with json as well as send a response and request.
+
+once you run the code in your command line all you will is a message to make sure is working like "Hello World".
--- server/app.js
+++ server/app.js
@@ -1,10 +1,22 @@
 // import files and packages up here
+var express = require('express');
 
 
 // create your express server below
-var app;
+var app = express();
+var json = require('./data.json');
+var morgan = require('morgan');
+var express = require('express');
 
 // add your routes and middleware below
+app.use(morgan('dev'))
+app.get('/', function(req, res){
+    res.status(200).send('Hello World');
+})
+
+app.get('/data', function(req, res){
+    res.send(json);
+})
 
 // finally export the express application
 module.exports = app;
